e77b29ed580c0a2ccbd01c24f4e70dd3b558006c
Carlos <carlostena@centroafuera.onmicrosoft.com>
2021-10-25 11:11:52 +0200

    Initial commit

---
+++ app/src/main/java/com/carlostena/discrollview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.carlostena.discrollview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
